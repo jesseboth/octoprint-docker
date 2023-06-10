108308ea07404cc7061d24296895e6d5653bdf86
Jesse Both <jesse9both@gmail.com>
2023-06-10 20:49:58 +0000

    Added tplink controls to allow docker to conncet to printer when printer is off

--- docker.sh
+++ docker.sh
@@ -2,6 +2,15 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CURDIR=$PWD
+TPLINK_IP="192.168.1.102"
+
 cd $SCRIPT_DIR
+
+
+./tplink-smartplug/tplink_smartplug.py -t $TPLINK_IP -c on
+sleep 2
 docker-compose up -d
+sleep 2
+./tplink-smartplug/tplink_smartplug.py -t $TPLINK_IP -c off
+
 cd $CURDIR
